e4852a1db4fe8bafd44fb48ce7902c6b8b1d1a5a
Tomas Lundström <tlu@vpd.se>
2013-12-27 23:47:12 +0100

    Initial commit

---
+++ FacebookStatistics/FacebookStatistics/Default.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FacebookStatistics._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1><%: Title %>.</h1>
+                <h2>Test Facebook API</h2>
+            </hgroup>
+            <p>
+                Test Facebook API. Test Facebook API. Test Facebook API
+                Test Facebook API. Test Facebook API. Test Facebook API
+
+            </p>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h3>Movies</h3>
+    <ol class="round">
+        <li class="one">
+            <h5>Getting Started</h5>
+            ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
+            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
+            <a href="http://go.microsoft.com/fwlink/?LinkId=245146">Learn more…</a>
+        </li>
+        <li class="one">
+            <h5>Add NuGet packages and jump-start your coding</h5>
+            NuGet makes it easy to install and update free libraries and tools.
+            <a href="http://go.microsoft.com/fwlink/?LinkId=245147">Learn more…</a>
+        </li>
+        <li class="one">
+            <h5>Find Web Hosting</h5>
+            You can easily find a web hosting company that offers the right mix of features and price for your applications.
+            <a href="http://go.microsoft.com/fwlink/?LinkId=245143">Learn more…</a>
+        </li>
+    </ol>
+    
+    <input id="testButton" type="button" value="Test API"/>
+     <div id="fb-root"></div> 
+<div id="login"></div> 
+<div id="test"></div> 
+<div id="movies"></div> 
+    
+<script src="Scripts/Facebook/displayMovies.js"  type="text/javascript"></script>  
+<script src="Scripts/Facebook/loadFaceBookSDK.js"  type="text/javascript"></script>   
+ <script>
+     window.fbAsyncInit = function () {
+         // init the FB JS SDK
+         FB.init({
+             appId: '367702730013608',
+             status: true, // check login status
+             cookie: true, // enable cookies 
+             xfbml: true, // parse XFBML
+             oauth: true
+         });
+
+         // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+         // for any authentication related change, such as login, logout or session refresh. This means that
+         // whenever someone who was previously logged out tries to log in again, the correct case below 
+         // will be handled. 
+         FB.getLoginStatus(function (response) {
+             if (response.status == "connected") {
+                 // logged in and connected user, someone you know
+                 get_friend_likes();
+                 document.getElementById('login').innerHTML
+                   = '<a href="#" onclick="FB.logout();">Logout</a><br/>';
+             } else {
+                 document.getElementById('login').innerHTML
+                   = '<fb:login-button show-faces="true" width="200"'
+                   + ' max-rows="1" perms="user_likes, friends_likes">'
+                   + '</fb:login-button>';
+                 FB.XFBML.parse();
+             }
+         });
+
+         FB.Event.subscribe('auth.login', function (response) {
+             if (response.status == "connected") {
+                 // logged in and connected user, someone you know
+                 get_friend_likes();
+                 document.getElementById('login').innerHTML
+                   = '<a href="#" onclick="FB.logout();">Logout</a><br/>';
+             } else {
+                 document.getElementById('login').innerHTML
+                   = '<fb:login-button show-faces="true" width="200"'
+                   + ' max-rows="1" perms="user_likes, friends_likes">'
+                   + '</fb:login-button>';
+                 FB.XFBML.parse();
+             }
+         });
+     };
+ </script>
+            <!--
+  Below we include the Login Button social plugin. This button uses the JavaScript SDK to
+  present a graphical Login button that triggers the FB.login() function when clicked. -->
+<%--<fb:login-button show-faces="true" width="200" max-rows="1" 
+perms="user_likes, friends_likes"></fb:login-button>--%>
+</asp:Content>
---
+++ FacebookStatistics/FacebookStatistics/Scripts/Facebook/displayMovies.js
@@ -0,0 +1,211 @@
+﻿// Response from FaceBook
+
+
+
+
+$(document).ready(function () {
+    $("#testButton").click(function() {
+        FB.api('/me', function (response) {
+            alert(response.name);
+        });
+    });
+});
+
+
+var movieList = new Array();
+var movieListSorted = new Array();
+var friendCount = 0;
+function showMovies() {
+    alert(movieList.length);
+}
+function compareMovies(movieA, movieB) {
+    if (movieA.name === movieB.name) return 0;
+    if (movieA.name > movieB.name) return 1;
+    return -1;
+}
+function popularMovies(movieA, movieB) {
+    return movieB.mCount - movieA.mCount;
+}
+function data_fetch_postproc() {
+    document.getElementById('test').innerHTML
+      = "Generating recommendations ... ";
+    movieList.sort(compareMovies);
+    // Now we have sorted list, dedupe and count
+    mCtr = 0;
+    for (i = 0; i < movieList.length; i++) {
+        var count = 0;
+        movieListSorted[mCtr] = movieList[i];
+        for (j = i; j < movieList.length; j++) {
+            if (movieList[i].name === movieList[j].name) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        i = i + count - 1;
+        movieListSorted[mCtr++].mCount = count;
+    }
+    var maxResults = 10;
+    if (movieListSorted.length < 10) {
+        maxResults = movieListSorted.length;
+    }
+    movieListSorted.sort(popularMovies);
+    document.getElementById('test').innerHTML = "";
+    for (i = 0; i < maxResults; i++) {
+        var newDiv = document.createElement("DIV");
+        newDiv.id = movieListSorted[i].id;
+        newDiv.innerHTML = movieListSorted[i].name + ' : Likes - '
+          + movieListSorted[i].mCount;
+        document.getElementById("movies").appendChild(newDiv);
+        FB.api('/' + movieListSorted[i].id, function (response) {
+            var newDiv = document.createElement("DIV");
+            newDiv.innerHTML = "<img src='" + response.picture + "'>"
+              + "</img><br/>";
+            if (response.link) {
+                newDiv.innerHTML += "<a href='" + response.link + "'>"
+                  + response.link + "</a><br/>";
+                newDiv.innerHTML += '<iframe src='
+                  + '"http://www.facebook.com/plugins/like.php?'
+                  + 'href=' + response.link + '&amp;layout=standard'
+                  + '&amp;show_faces=true&amp;'
+                  + 'width=450&amp;action=like&amp;'
+                  + 'colorscheme=light&amp;height=80"'
+                  + 'scrolling="no" frameborder="0" style="'
+                  + 'border:none; overflow:hidden;'
+                  + 'width:450px; height:80px;"'
+                  + 'allowTransparency="true"></iframe><br/>';
+            }
+            document.getElementById(response.id).appendChild(newDiv);
+        });
+    }
+}
+function get_friend_likes() {
+    document.getElementById('test').innerHTML = "Requesting "
+      + "data from Facebook ... ";
+    FB.api('/me/friends', function (response) {
+        friendCount = response.data.length;
+        for (i = 0; i < response.data.length; i++) {
+            friendId = response.data[i].id;
+            FB.api('/' + friendId + '/movies', function (response) {
+                movieList = movieList.concat(response.data);
+                friendCount--;
+                document.getElementById('test').innerHTML = friendCount
+                  + " friends to go ... ";
+                if (friendCount === 0) { data_fetch_postproc(); };
+            });
+        }
+    });
+}
+
+//window.fbAsyncInit = function () {
+//    FB.init({
+//        appId: '367702730013608',
+//        status: true, // check login status
+//        cookie: true, // enable cookies to allow the server to access the session
+//        xfbml: true  // parse XFBML
+//    });
+
+//    // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+//    // for any authentication related change, such as login, logout or session refresh. This means that
+//    // whenever someone who was previously logged out tries to log in again, the correct case below 
+//    // will be handled. 
+//    FB.Event.subscribe('auth.authResponseChange', function (response) {
+//        // Here we specify what we do with the response anytime this event occurs. 
+//        if (response.status === 'connected') {
+//            // The response object is returned with a status field that lets the app know the current
+//            // login status of the person. In this case, we're handling the situation where they 
+//            // have logged in to the app.
+//            testAPI();
+//        } else if (response.status === 'not_authorized') {
+//            // In this case, the person is logged into Facebook, but not into the app, so we call
+//            // FB.login() to prompt them to do so. 
+//            // In real-life usage, you wouldn't want to immediately prompt someone to login 
+//            // like this, for two reasons:
+//            // (1) JavaScript created popup windows are blocked by most browsers unless they 
+//            // result from direct interaction from people using the app (such as a mouse click)
+//            // (2) it is a bad experience to be continually prompted to login upon page load.
+//            FB.login();
+//        } else {
+//            // In this case, the person is not logged into Facebook, so we call the login() 
+//            // function to prompt them to do so. Note that at this stage there is no indication
+//            // of whether they are logged into the app. If they aren't then they'll see the Login
+//            // dialog right after they log in to Facebook. 
+//            // The same caveats as above apply to the FB.login() call here.
+//            FB.login();
+//        }
+//    });
+//};
+
+//// Load the SDK asynchronously
+//(function (d) {
+//    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+//    if (d.getElementById(id)) { return; }
+//    js = d.createElement('script'); js.id = id; js.async = true;
+//    js.src = "//connect.facebook.net/en_US/all.js";
+//    ref.parentNode.insertBefore(js, ref);
+//}(document));
+
+//// Here we run a very simple test of the Graph API after login is successful. 
+//// This testAPI() function is only called in those cases. 
+//function testAPI() {
+//    console.log('Welcome!  Fetching your information.... ');
+//    FB.api('/me', function (response) {
+//        console.log('Good to see you, ' + response.name + '.');
+//    });
+//}
+
+////window.fbAsyncInit = function () {
+////    FB.init({
+////        appId: '367702730013608',
+////        status: true,
+////        cookie: true,
+////        xfbml: true,
+////        oauth: true
+////    });
+
+////    showLoader(true);
+
+////    function updateButton(response) {
+////        button = document.getElementById('fb-auth');
+////        userInfo = document.getElementById('user-info');
+
+////        if (response.authResponse) {
+////            //user is already logged in and connected
+////            FB.api('/me', function (info) {
+////                login(response, info);
+////            });
+
+////            button.onclick = function () {
+////                FB.logout(function (response) {
+////                    logout(response);
+////                });
+////            };
+////        } else {
+////            //user is not connected to your app or logged out
+////            button.innerHTML = 'Login';
+////            button.onclick = function() {
+////                showLoader(true);
+////                FB.login(function(response) {
+////                    if (response.authResponse) {
+////                        FB.api('/me', function(info) {
+////                            login(response, info);
+////                        });
+////                    } else {
+////                        //user cancelled login or did not grant authorization
+////                        showLoader(false);
+////                    }
+////                }, { scope: 'email,user_birthday,status_update,publish_stream,user_about_me' });
+////            }
+////        }
+////    }
+
+////    // run once with current status and whenever the status changes
+////    FB.getLoginStatus(updateButton);
+////    FB.Event.subscribe('auth.statusChange', updateButton);
+////};
+////(function () {
+////    var e = document.createElement('script'); e.async = true;
+////    e.src = document.location.protocol
+////        + '//connect.facebook.net/en_US/all.js';
+////    document.getElementById('fb-root').appendChild(e);
+////}());
